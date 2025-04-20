061559d9da9f93337b2e05f71d0b774015d528bf
AktsT <akts.tlga@gmail.com>
2025-04-21 01:20:10 +0300

    Detailed and nearly finished US_607

--- src/Utility/ReUsableMethods.java
+++ src/Utility/ReUsableMethods.java
@@ -1,26 +1,20 @@
 package Utility;
 
 import Pages.CourseDetails_POM;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class ReUsableMethods extends BaseDriverParameter{
     CourseDetails_POM courseDetails = new CourseDetails_POM();
 
-    public void throughLearnMoreButtons(WebElement element,WebElement element2){
+    public void throughLearnMoreButtons(WebElement element,WebElement logoElement){
         action.scrollToElement(element).build().perform();
         wait.until(ExpectedConditions.elementToBeClickable(element));
         wait.until(ExpectedConditions.elementToBeClickable(element));
         element.click();
-        wait.until(ExpectedConditions.visibilityOf(element2));
-        Assert.assertTrue(element2.isDisplayed(),"Logo Bulundu");
-        driver.navigate().back();
+        wait.until(ExpectedConditions.visibilityOf(logoElement));
+        Assert.assertTrue(logoElement.isDisplayed(),"Logo Bulundu");
+        logoElement.click();
     }
 }
