39942ca7bc55d349d56ead493acee529a57298f7
AktsT <akts.tlga@gmail.com>
2025-04-21 00:58:30 +0300

    Detailed and nearly finished US_607

--- src/NavigationThroughCourseDetails.java
+++ src/NavigationThroughCourseDetails.java
@@ -1,11 +1,19 @@
+import Pages.CourseDetails_POM;
 import Utility.BaseDriverParameter;
+import Utility.ReUsableMethods;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class NavigationThroughCourseDetails extends BaseDriverParameter {
+    ReUsableMethods usableMethods=new ReUsableMethods();
+
     @Test(priority = 1)
     @Parameters("BrowserType")
     public void courseDetails(){
+        CourseDetails_POM courseDetails=new CourseDetails_POM();
         driver.get("https://techno.study/");
+        usableMethods.throughLearnMoreButtons(courseDetails.sdetButton,courseDetails.technoStudyLogo);
+        usableMethods.throughLearnMoreButtons(courseDetails.androidBootcampButton, courseDetails.technoStudyLogo);
+        usableMethods.throughLearnMoreButtons(courseDetails.dataScienceButton,courseDetails.technoStudyLogo);
     }
 }
--- src/Pages/CourseDetails_POM.java
+++ src/Pages/CourseDetails_POM.java
@@ -1,6 +1,7 @@
 package Pages;
 
 import Utility.BaseDriverParameter;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -9,4 +10,15 @@ public class CourseDetails_POM extends BaseDriverParameter {
         PageFactory.initElements(BaseDriverParameter.driver, this);
     }
 
+    @FindBy(css = "a[class='tn-atom'][href='https://techno.study/data-science-bootcamps']")
+    public WebElement dataScienceButton;
+
+    @FindBy(css = "a[class='tn-atom'][href='https://techno.study/android-bootcamp']")
+    public WebElement androidBootcampButton;
+
+    @FindBy(css = "a[class='tn-atom'][href='https://techno.study/sdet-bootcamp']")
+    public WebElement sdetButton;
+
+    @FindBy(css = "img[imgfield='img']")
+    public WebElement technoStudyLogo;
 }
