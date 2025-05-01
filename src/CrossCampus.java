745874bb854f65be76d25af9e66a278f6f372f22
meertsarac <meertsarac@gmail.com>
2025-05-01 23:28:05 +0300

    Add crossCampus test method

--- src/CrossCampus.java
+++ src/CrossCampus.java
@@ -1,6 +1,11 @@
 import Utility.BaseDriverParameter;
 import Utility.ReUsableMethods;
+import org.testng.annotations.Test;
 
 public class CrossCampus extends BaseDriverParameter {
     ReUsableMethods usableMethods = new ReUsableMethods();
+    @Test
+    public void crossCampus(){
+
+    }
 }
