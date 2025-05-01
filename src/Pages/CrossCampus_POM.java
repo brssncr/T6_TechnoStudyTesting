ba51b74186d9468696765a1cdace300606a0fc8c
meertsarac <meertsarac@gmail.com>
2025-05-01 23:01:53 +0300

    Initialize web elements in CrossCampus_POM using PageFactory

--- src/Pages/CrossCampus_POM.java
+++ src/Pages/CrossCampus_POM.java
@@ -1,6 +1,11 @@
 package Pages;
 
 import Utility.BaseDriverParameter;
+import org.openqa.selenium.support.PageFactory;
 
 public class CrossCampus_POM extends BaseDriverParameter {
+    public CrossCampus_POM() {
+        PageFactory.initElements(BaseDriverParameter.driver, this);
+    }
+
 }
