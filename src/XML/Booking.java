521fb7ba2d641c028b2510cdd998d460013b7e5a
mert <mertcanat91@gmail.com>
2025-05-02 00:32:53 +0300

    Booking hata veren kodlar düzeltildi

--- src/XML/Booking.java
+++ src/XML/Booking.java
@@ -29,39 +29,43 @@ public class Booking extends BaseDriverParameter {
         wait.until(ExpectedConditions.visibilityOf(emailButton));
         emailButton.sendKeys("test@gmail.com");
 
-        WebElement flagDropdown = driver.findElement(By.cssSelector(".flag-dropdown.selected-flag"));
-        wait.until(ExpectedConditions.visibilityOf(flagDropdown));
-        flagDropdown.click();
+        WebElement arrowElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='t-input-phonemask__select-flag']")));
+        actions.moveToElement(arrowElement).click().perform();
 
-        WebElement andorraOption = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[@data-country-code='ad']")));
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500);");
+
+        WebElement andorraOption = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='t-input-phonemask__options-item' and @data-phonemask-country-code='ad']")));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block:'center'});", andorraOption);
+        wait.until(ExpectedConditions.elementToBeClickable(andorraOption));
         actions.moveToElement(andorraOption).click().perform();
 
-        WebElement phoneNumber = driver.findElement(By.cssSelector(".PhoneInputInput"));
+        WebElement phoneNumber =driver.findElement(By.xpath("//input[@id='input_1661174990146']"));
         wait.until(ExpectedConditions.elementToBeClickable(phoneNumber));
         phoneNumber.sendKeys("999999");
 
-        WebElement chooseCountryDropdown = driver.findElement(By.xpath("//div[contains(text(), 'Choose your country')]"));
+        WebElement chooseCountryDropdown = driver.findElement(By.xpath("//select[@name='country']"));
         wait.until(ExpectedConditions.elementToBeClickable(chooseCountryDropdown));
         chooseCountryDropdown.click();
 
-        WebElement countryOption = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='t-select__option' and text()='Andorra']")));
+        WebElement countryOption = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[text()='Andorra']")));
 
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", countryOption);
         wait.until(ExpectedConditions.elementToBeClickable(countryOption)).click();
 
-        WebElement course = driver.findElement(By.xpath("//div[contains(text(), 'Choose your course')]"));
+        WebElement course = driver.findElement(By.xpath("//select[@name='course']"));
         wait.until(ExpectedConditions.elementToBeClickable(course));
         course.click();
 
-        WebElement sdetOption = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='t-select__option' and text()='SDET']")));
+        WebElement sdetOption = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[text()='SDET']")));
         wait.until(ExpectedConditions.elementToBeClickable(sdetOption));
         sdetOption.click();
 
-        WebElement checkBox = driver.findElement(By.cssSelector("input[type='checkbox']"));
+        WebElement checkBox = driver.findElement(By.xpath("//label[contains(@class,'checkbox__control')]"));
         wait.until(ExpectedConditions.elementToBeClickable(checkBox));
         checkBox.click();
 
-        WebElement bookCallBtn = driver.findElement(By.xpath("//button[countains(text),'Book a Call']"));
+        WebElement bookCallBtn = driver.findElement(By.xpath("//button[text()='Book a Call']"));
         wait.until(ExpectedConditions.elementToBeClickable(bookCallBtn));
         bookCallBtn.click();
     }
