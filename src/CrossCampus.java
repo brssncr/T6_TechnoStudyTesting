65897ce70b02c0add67136535ac53eaf696e1f53
meertsarac <meertsarac@gmail.com>
2025-05-02 04:13:20 +0300

    Add assertions to verify URL and login panel visibility

--- src/CrossCampus.java
+++ src/CrossCampus.java
@@ -2,6 +2,7 @@ import Pages.CrossCampus_POM;
 import Utility.BaseDriverParameter;
 import Utility.ReUsableMethods;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CrossCampus extends BaseDriverParameter {
@@ -12,6 +13,12 @@ public class CrossCampus extends BaseDriverParameter {
         driver.get("https://techno.study/");
         wait.until(ExpectedConditions.elementToBeClickable(crossCampus.signInButton));
         crossCampus.signInButton.click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("campus"), "URL 'dashboard' içermiyor!");
+        wait.until(ExpectedConditions.visibilityOf(crossCampus.loginPanel));
+        Assert.assertTrue(crossCampus.loginPanel.isDisplayed(),"Login paneli görünmüyor.");
+
+
 
 
     }
