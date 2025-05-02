17c8da07b6b246b61fd3301e66976abeeb82aed3
meertsarac <meertsarac@gmail.com>
2025-05-02 03:25:55 +0300

    Wait for and click the signInButton

--- src/CrossCampus.java
+++ src/CrossCampus.java
@@ -1,17 +1,17 @@
 import Pages.CrossCampus_POM;
 import Utility.BaseDriverParameter;
 import Utility.ReUsableMethods;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class CrossCampus extends BaseDriverParameter {
-    ReUsableMethods usableMethods = new ReUsableMethods();
-
     @Test
     public void crossCampus() {
-        CrossCampus_POM login = new CrossCampus_POM();
+        CrossCampus_POM crossCampus = new CrossCampus_POM();
 
         driver.get("https://techno.study/");
-
+        wait.until(ExpectedConditions.elementToBeClickable(crossCampus.signInButton));
+        crossCampus.signInButton.click();
 
 
     }
